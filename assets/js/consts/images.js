bc9d8673a06b8fed3119d26a8209945738ea4e9d
denis-mixin <73473618+denis-mixin@users.noreply.github.com>
2021-11-30 11:33:54 +0000

    Deploying to gh-pages from @ darkfrogstudio/cantina-royale-website-template@9843ce4d8feebe07ab6a0c4d5edb615e579951a4 🚀

---
+++ assets/js/consts/images.js
@@ -0,0 +1,95 @@
+export const images = {
+  
+    play: "/assets/images/home/play.svg",
+    close: "/assets/images/home/close.svg",
+    appLoader: "/assets/images/home/app-loader.png",
+    appLoaderOverlay: "/assets/images/home/app-loader-overlay.png",
+    appLoaderBg: "/assets/images/home/app-loader-bg.png",
+    indicatorLine: "/assets/images/home/indicator-line.png",
+    shared: {
+        curve1: "/assets/images/home/shared/curve-1.png",
+        curve2: "/assets/images/home/shared/curve-2.png",
+        curve3: "/assets/images/home/shared/curve-3.png",
+    },
+    main: {
+            bg: "/assets/images/home/main/bg.png",
+            overlay: "/assets/images/home/main/overlay.png",
+    },
+    apes: {
+        bg: "/assets/images/home/apes/bg.png",
+        overlay: "/assets/images/home/apes/overlay.png",
+        coins: "/assets/images/home/apes/coins.png",
+        ape1: "/assets/images/home/apes/ape-1.png",
+        ape2: "/assets/images/home/apes/ape-2.png",
+},
+    cantina: {
+        bg: "/assets/images/home/cantina/bg.png",
+        overlay: "/assets/images/home/cantina/overlay.png",
+    },
+    navbar: {
+        logo: "/assets/images/home/navbar/logo.png",
+        comingSoon: "/assets/images/home/navbar/coming-soon.svg",
+    },
+    species:{
+        bg: "/assets/images/home/species/bg.png",
+        overlay: "/assets/images/home/species/overlay.png",
+        globes: {
+            globe1: "/assets/images/home/species/globes/globe1.png",
+            globe2: "/assets/images/home/species/globes/globe2.png",
+            globe3: "/assets/images/home/species/globes/globe3.png",
+            globe4: "/assets/images/home/species/globes/globe4.png",
+        },
+        figures: {
+            figure1: "/assets/images/home/species/figures/figure1.png",
+            figure2: "/assets/images/home/species/figures/figure2.png",
+            figure3: "/assets/images/home/species/figures/figure3.png",
+            figure4: "/assets/images/home/species/figures/figure4.png",
+            figure5: "/assets/images/home/species/figures/figure5.png",
+        }
+    },
+    variations: {
+        globe: "/assets/images/home/variations/globe.png",
+        videoShape: "/assets/images/home/variations/video-shape.png",
+        overlay: "/assets/images/home/variations/overlay.png",
+        circle: "/assets/images/home/variations/circle.png",
+        arrow: "/assets/images/home/variations/arrow.svg",
+        bg: "/assets/images/home/variations/bg.png",
+    },
+    pve:{
+        bg: "/assets/images/home/pve/bg.png",
+        videoPreview: "/assets/images/home/pve/video-preview.png",
+        overlay: "/assets/images/home/pve/overlay.png",
+    },
+    pvp:{
+        bg: "/assets/images/home/pvp/bg.png",
+        videoPreview: "/assets/images/home/pvp/video-preview.png",
+        overlay: "/assets/images/home/pvp/overlay.png",
+    },
+    scholarships:{
+        bg: "/assets/images/home/scholarships/bg.png",
+        overlay: "/assets/images/home/scholarships/overlay.png",
+    },
+    partners:{
+        bg: "/assets/images/home/partners/bg.png",
+        overlay: "/assets/images/home/partners/overlay.png",
+        companies: {
+            atlas: "/assets/images/home/partners/companies/atlas.png",
+            avalanche: "/assets/images/home/partners/companies/avalanche.png",
+            coinMarketCap: "/assets/images/home/partners/companies/coin-market-cap.png",
+            crypto: "/assets/images/home/partners/companies/crypto.png",
+            meta: "/assets/images/home/partners/companies/meta.png",
+            opensea: "/assets/images/home/partners/companies/opensea.png",
+            sega: "/assets/images/home/partners/companies/sega.png",
+            star: "/assets/images/home/partners/companies/star.png",
+            switch: "/assets/images/home/partners/companies/switch.png",
+        },
+        community: {
+            discord: "/assets/images/home/partners/community/discord.svg",
+            telegram: "/assets/images/home/partners/community/telegram.svg",
+            social: "/assets/images/home/partners/community/social.svg",
+            youtube: "/assets/images/home/partners/community/youtube.svg",
+            twitter: "/assets/images/home/partners/community/twitter.svg",
+        }
+    }
+  };
+  
---
+++ assets/js/index.js
@@ -0,0 +1,97 @@
+import { pagesIndexes, sections } from "./consts/index.js"
+import mainPage from './pages/main.js';
+import indicator from './indicator/index.js'
+import speciesPage from './pages/species.js'
+import pvePage from "./pages/pve.js";
+import pvpPage from "./pages/pvp.js";
+import uiUtil from "./utils/ui.js";
+import variationsPage from "./pages/variations.js";
+import apesPage from "./pages/apes.js";
+
+
+let allowScroll  = true
+const initFullPage = () => {
+    $("#fullpage").fullpage({
+        controlArrows: false,
+        onLeave: changePage,
+    });
+}
+
+
+const changePage = (_val, page) => {
+    if(!allowScroll){
+        return 
+    }
+    const index = page - 1;
+    $.fn.fullpage.setAllowScrolling(false)
+    onPageChanged(index)
+    location.replace(sections[index].link)
+    sketch.change(index, () => $.fn.fullpage.setAllowScrolling(true));
+};
+
+
+
+const onPageChanged = async (index) => {
+    indicator.changeActiveSection(index)
+    uiUtil.overlayHandler(index)
+    uiUtil.changeNavbarStyle(index)
+    uiUtil.toggleIndicator(index > 0)
+
+    clearIntervals()
+    stopVideos()
+
+    switch (index) {
+        case pagesIndexes.variations:
+            variationsPage.onPageInView()
+            break;
+        case pagesIndexes.main:
+            mainPage.onPageInView()
+            break;
+        case pagesIndexes.pve:
+            pvePage.onPageInView()
+            break;
+        case pagesIndexes.pvp:
+            pvpPage.onPageInView()
+            break;
+        case pagesIndexes.apes:
+            apesPage.onPageInView()
+            break;
+        default:
+    }
+}
+
+
+const stopVideos = () => {
+    mainPage.stopVideo()
+    pvePage.stopVideo()
+    pvpPage.stopVideo()
+    variationsPage.stopVideo()
+    
+
+}
+
+
+const clearIntervals = () => {
+    variationsPage.clearInterval()
+    apesPage.clearIntervals()
+}
+
+
+const resetUrl = () => {
+    history.pushState("", document.title, window.location.pathname
+        + window.location.search);
+}
+
+
+export const init = async () => {
+    initFullPage()
+    speciesPage.createSlider()
+    resetUrl()
+    indicator.addEvents()
+    await uiUtil.hideAppLoader()
+    mainPage.playVideo()
+}
+
+onload = () => {
+    init()
+}
